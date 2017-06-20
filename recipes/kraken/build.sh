aa906d094bea37826c3a58016b4e66ec7c726d22
Simon Ye <sye737@gmail.com>
2017-06-20 12:35:41 -0400

    Kraken fork recipe

--- recipes/kraken/build.sh
+++ recipes/kraken/build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-mkdir -p "$PREFIX/libexec" "$PREFIX/bin"
-
 chmod u+x install_kraken.sh
-./install_kraken.sh "$PREFIX/libexec"
+./install_kraken.sh "$PREFIX"
 for bin in kraken kraken-build kraken-filter kraken-mpa-report kraken-report kraken-translate; do
-    sed -i.bak 's|#!/usr/bin/perl|#!/usr/bin/env perl|' $PREFIX/libexec/$bin
-    rm -f $PREFIX/libexec/$bin.bak
-    chmod +x "$PREFIX/libexec/$bin"
-    ln -s "$PREFIX/libexec/$bin" "$PREFIX/bin/$bin"
+    sed -i.bak 's|#!/usr/bin/perl|#!/usr/bin/env perl|' "$PREFIX/libexec/$bin"
+    rm -f $PREFIX/bin/$bin.bak
 done
-
